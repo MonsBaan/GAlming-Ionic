f7f9e831df39555b7015996e7aeb8bd2bc5cd509
MonsBaan <ibai3553@gmail.com>
2021-10-22 14:18:35 +0200

    Side Menu Login y Main Slider

---
+++ GAlming-Ionic/src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private appComponent:AppComponent) { }
+  items = ["asafs","bafsaf","c","dwfafw","eawfafw","ffwaf","asafs","bafsaf",
+  "c","dwfafw","eawfafw","ffwaf","asafs","bafsaf","c","dwfafw","eawfafw","ffwaf"
+  ,"asafs","bafsaf","c","dwfafw","eawfafw","ffwaf","asafs","bafsaf","c","dwfafw",
+  "eawfafw","ffwaf","asafs","bafsaf","c","dwfafw","eawfafw","ffwaf","asafs","bafsaf",
+  "c","dwfafw","eawfafw","ffwaf","asafs","bafsaf","c","dwfafw","eawfafw","ffwaf","asafs",
+  "bafsaf","c","dwfafw","eawfafw","ffwaf","asafs","bafsaf","c","dwfafw","eawfafw","ffwaf",
+  "asafs","bafsaf","c","dwfafw","eawfafw","ffwaf","asafs","bafsaf","c","dwfafw","eawfafw",
+  "ffwaf","asafs","bafsaf","c","dwfafw","eawfafw","ffwaf",];
+
+
+  ngOnInit() {
+    localStorage.setItem("login", "0")
+    console.log(localStorage.getItem("login"))
+
+    /*if (localStorage.getItem("login")=="1") {
+      this.appComponent.menuLogged();
+
+    }else{
+      this.appComponent.menu();
+
+    }*/
+  }
+
+
+
+
+
+}
