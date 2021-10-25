cd2eb2e129e80c8bbca1591b851b0748e8eaf541
iMario44 <90375304+iMario44@users.noreply.github.com>
2021-10-25 13:59:34 +0200

    Perfil Terminado

--- GAlming-Ionic/src/app/perfil/perfil.page.ts
+++ GAlming-Ionic/src/app/perfil/perfil.page.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+import { modalController } from '@ionic/core';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage implements OnInit
+{
+  modo = true;
+  constructor(){ }
+  
+  /** DATOS  */
+  usuario = {nombre:"Mario" , 
+             apellido1:"Torres",
+             apellido2:"Cid",
+             contrasena:'Almi123',
+             dni:'574625434K',
+             direccion:'Colon 22',
+             email:'Mario@gmail.com',
+             ciudad:'Bilbao'
+            };
 
-  constructor() { }
+  ngOnInit()
+  {
 
-  usuario = {nombre:"Mario" , apellido:"Torres" , contrasena:'Almi123'};
-
-  ngOnInit() {
   }
 
+  mostrar()
+  {
+    this.modo = false
+  }
+
+  enviar()
+  {
+    this.modo = true
+    
+  }
 }
