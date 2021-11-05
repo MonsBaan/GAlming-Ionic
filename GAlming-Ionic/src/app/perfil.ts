d603f15aa30dbf6993115f5f53bf39af02263b22
Inushin <aitorsobera@gmail.com>
2021-11-05 12:38:49 +0100

    previo Merge. Perfil OK

---
+++ GAlming-Ionic/src/app/perfil.ts
@@ -0,0 +1,13 @@
+export class Perfil {
+    usuDni: string;
+    usuNombre: string;
+    usuApellido1: string;
+    usuApellido2: string;
+    usuDireccion: string;
+    usuPass: string;
+    usuEmail: string;
+    usuFoto: string;
+    usuCiudad: string;
+}
+
+
--- GAlming-Ionic/src/app/perfil/perfil.page.ts
+++ GAlming-Ionic/src/app/perfil/perfil.page.ts
@@ -1,41 +1,82 @@
+import { PerfilService } from './../servicios/perfil.service';
 import { Component, OnInit } from '@angular/core';
 import { disableDebugTools } from '@angular/platform-browser';
 import { modalController } from '@ionic/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit
-{
+export class PerfilPage implements OnInit {
   modo = true;
-  constructor(){ }
-  
-  /** DATOS  */
-  usuario = {nombre:"Mario" , 
-             apellido1:"Torres",
-             apellido2:"Cid",
-             contrasena:'Almi123',
-             dni:'574625434K',
-             direccion:'Colon 22',
-             email:'Mario@gmail.com',
-             ciudad:'Bilbao'
-            };
 
-  ngOnInit()
-  {
+
+  /** DATOS  */
+  public usuario = {
+    usuDni: "574625434K",
+    usuNombre: "Mario",
+    usuApellido1: "Torres",
+    usuApellido2: "Cid",
+    usuDireccion: 'Colon 22',
+    usuPass: 'Almi123',
+    usuEmail: 'Mario@gmail.com',
+    usuFoto: 'FOTO',
+    usuCiudad: 'Bilbao'
+  };
+
+  constructor(private perfilService: PerfilService, public loadingController: LoadingController) { }
+
+  async cargarPerfil() {
+    localStorage.setItem('usuId', "4");
+
+    const loading = await this.loadingController.create({
+      message: 'Cargando...'
+    });
+
+    await loading.present();
+    await this.perfilService.cargarPerfil(this.usuario)
+    .subscribe(res => {
+      console.log(res[0]);
+      loading.dismiss();
+      if (res[0] == 0 || res[0] == null) {
+        alert("Usuario inexistente")
+      }
+      else{
+      this.usuario = res[0];
+      }
+    }, err => {
+      console.log(err);
+      loading.dismiss();
+    }
+    
+    )};
+
+    
+
+
+
+  ngOnInit() {
+
+    this.cargarPerfil();
 
   }
 
-  mostrar()
-  {
+  mostrar() {
     this.modo = false
   }
 
-  enviar()
-  {
+  enviar() {
     this.modo = true
-    
+    console.log(this.usuario);
+    this.perfilService.actualizarPerfil(this.usuario)
+    .subscribe(res=>{
+      console.log(res);
+      this.usuario = res;
+    }, err =>{
+      console.log(err);
+    })
+    this.cargarPerfil();
   }
 }
